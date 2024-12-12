b2f25a25ca61fa5b4702f5a2c5995c6e79eb8c0e
Chilufya Mutale <cmutaledev@gmail.com>
2024-12-12 00:20:05 -0600

    fixing build errors

--- apps/little-horizons-daycare/components/Footer.tsx
+++ apps/little-horizons-daycare/components/Footer.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
-import { Button } from './ui/button';
+import { Button } from './ui/button'
 
 const quickLinks = [
     {
