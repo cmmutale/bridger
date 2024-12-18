b82650651211ae0d9a55e08fbd05fa19f6d5a74e
Chilufya Mutale <cmutaledev@gmail.com>
2024-12-18 13:02:00 -0600

    fixing build errors

--- apps/little-horizons-daycare/app/page.tsx
+++ apps/little-horizons-daycare/app/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from '@/components/header'
 import Programs from '@/components/landing/Programs'
 import AboutIntro from '@/components/landing/AboutIntro'
 import Masthead from '@/components/landing/Masthead'
@@ -8,8 +7,6 @@ import TeachersIntro from '@/components/landing/TeachersIntro'
 import VirtualTour from '@/components/landing/VirtualTour'
 import Testimonials from '@/components/landing/Testimonials'
 import FAQ from '@/components/landing/FAQ'
-import Cta from '@/components/landing/Cta'
-import Footer from '@/components/Footer'
 
 
 export default function page() {
