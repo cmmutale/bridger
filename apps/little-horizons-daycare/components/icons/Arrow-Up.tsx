774db52e887394f8ef20a57cce6251b5da97b675
Chilufya Mutale <cmutaledev@gmail.com>
2025-03-27 18:44:48 -0500

    removed project

--- apps/little-horizons-daycare/components/icons/Arrow-Up.tsx
+++
@@ -1,5 +0,0 @@
-const ArrowUp = (props: any) => (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 14 14" id="Arrow-Up-1--Streamline-Core" ><desc>{"Arrow Up 1 Streamline Icon: https://streamlinehq.com"}</desc><g id="arrow-up-1--arrow-up-keyboard"><path id="Vector" strokeLinecap="round" strokeLinejoin="round" d="M6 11.571428571428573V0.4285714285714286" strokeWidth={2} /><path id="Vector_2"  strokeLinecap="round" strokeLinejoin="round" d="M9 3.428571428571429 6 0.4285714285714286 3 3.428571428571429" strokeWidth={2} /></g></svg>
-)
-
-export default ArrowUp;
